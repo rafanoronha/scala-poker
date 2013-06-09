1454705ce4e006bc12f2910f5ad75500ab68db67
rafanoronha <rafanoronha@gmail.com>
2013-06-09 14:24:17 -0300

    refactoring

--- src/main/scala/spoker/BettingRound.scala
+++ src/main/scala/spoker/BettingRound.scala
@@ -11,25 +11,22 @@ package object betting {
   class BettingRound private(players: Iterator[Player], val bettingActionGotPlaced: Boolean = false) {
     val inTurn = players.next()
 
-    def place(action: Action) = {
-      if (inTurn != action.player) {
-        throw new OutOfTurnException
-      }
-      if (action.isInstanceOf[Check]) {
-        if (action.player.isInstanceOf[BigBlindPlayer]) {
-          if (bettingActionGotPlaced) {
-            throw new CantCheckException
-          }
-        } else {
-          throw new NonBigBlindCheckException
-        }
-      }
-      if (players.hasNext) {
-        new BettingRound(players, bettingActionGotPlaced || action.isInstanceOf[Raise])
-      } else {
-        this
-      }
+    def place(action: Action) = (action, action.player, bettingActionGotPlaced, players.hasNext) match {
+      case (_, OutOfTurn(), _, _) => throw new OutOfTurnException
+      case (_: Check, NonBigBlind(), _, _) => throw new NonBigBlindCheckException
+      case (_: Check, _, true, _) => throw new CantCheckException
+      case (_, _, _, true) => new BettingRound(players, bettingActionGotPlaced || action.isInstanceOf[Raise])
+      case _ => this
     }
+
+    object OutOfTurn {
+      def unapply(p: Player) = p != inTurn
+    }
+
+    object NonBigBlind {
+      def unapply(p: Player) = !p.isInstanceOf[BigBlindPlayer]
+    }
+
   }
 
   sealed abstract class Action(val player: Player)
