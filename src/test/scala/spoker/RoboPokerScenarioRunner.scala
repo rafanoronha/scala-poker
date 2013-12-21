11f4e1b02b8e1ff06d75b624315d4a8f87f3862f
rafanoronha <rafanoronha@gmail.com>
2013-12-21 14:54:43 -0200

    robopoker experiments related to #28

---
+++ src/test/scala/spoker/RoboPokerScenarioRunner.scala
@@ -0,0 +1,63 @@
+package spoker.spec
+
+import scala.io.Source
+import scala.xml.XML
+import scala.util.Try
+import org.scalatest.FunSpec
+import spoker._
+import spoker.betting._
+import spoker.betting.stack._
+import scala.xml.Node
+import org.scalatest.matchers.ShouldMatchers
+
+class RoboPokerSpec extends FunSpec with ShouldMatchers {
+  describe("The given scenario") {
+    it("should pass") {
+      val file = sys.props.get("robopokerscenario")
+      file.foreach(f => {
+        val source = (Source.fromFile(f, "utf-8"))
+        val scenario = XML.loadString(source.getLines.mkString)
+        val players = {
+          val button = (scenario \\ "table" \ "@button").text
+          def isNotButton(n: Node) = (n \ "@sit").text != button
+          val ps = (scenario \\ "table" \\ "player").span(isNotButton)
+          (ps._2 ++ ps._1).map(p => (Player((p \ "@name").text), (p \ "@stack").text.toInt))
+        }
+        val blinds = {
+          val posts = (scenario \\ "post").map(b => (b \ "@amount").text.toInt)
+          Blinds(posts.head, posts.tail.head)
+        }
+        val rounds = (scenario \\ "round").map(r => (r \\ "action")).filterNot(_.isEmpty).map(_.map(
+          a => {
+            val betterName = (a \ "@player").text
+            val action = (a \ "@type").text match {
+              case "fold" => Fold
+              case "check" => Check
+              //case "bet" =>
+              case "call" => Call
+              case "raise" => Raise(20)//Raise((a \ "@amount").text.toInt)
+              //case "allin" =>
+            }
+            (action, betterName)
+          }))
+        println(blinds)
+        println(players)
+        println(rounds)
+        var table = Table(players.map(_._1), blinds).newHand
+        rounds.foreach(r => {
+          r.foreach(a => {
+            val betterAction = (a._1, table.currentRound.get.betters.find(_.player.name == a._2).get)
+            println("placing action " + betterAction)
+            table.place(betterAction)
+          })
+          if (!(rounds.head :: rounds.last :: Nil).contains(r))
+            table = table.nextRound
+        })
+        players.foreach(p => {
+          val better = table.betters.get.find(_.player == p._1).get
+          p._2 should be(better.stack)
+        })
+      })
+    }
+  }
+}
